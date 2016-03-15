f2175dee31ee4cf7fce308414bc1b38e68000fb9
Nipitpon Chantada <nppi3enz@gmail.com>
2016-03-15 13:51:07 +0700

    can show first

--- main2.py
+++ main2.py
@@ -1,96 +1,104 @@
-
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-class Example(QtGui.QWidget):
+class DollyProject(QWidget):
 
    def __init__(self):
-        super(Example, self).__init__()
-
+        super(DollyProject, self).__init__()
         self.initUI()
+
+    #    self.stacked_layout = QStackedLayout() #create layout window
+    #    self.stacked_layout.addWidget(self.create_mainwindow_layout)
+    #    self.stacked_layout.setCurrentIndex(1)
+
+    #    self.central_widget = QWidget()
+    #    self.central_widget.setLayout(self.stacked_Layout)
+    #    self.setCentralWidget(self.central_widget)
+
 
    def initUI(self):
 
-        palette = QtGui.QPalette()
-        palette.setColor(QtGui.QPalette.Background,QtGui.QColor(36, 36, 36))
+        palette = QPalette()
+        palette.setColor(QPalette.Background,QColor(36, 36, 36))
         self.setPalette(palette)
-        self.create_mainwindows_layout()
+        self.create_mainwindow_layout()
+        #self.howto1()
+        #self.initial_layout = QVBoxLayout()
+        #self.initial_layout.addWidget(self.create_mainwindow_layout)
 
-        #self.stacked_layout = QtCore.QStackedLayout()
-        #self.stacked_layout.addWidget(self.create_mainwindows_layout)
+        #self.select_crop_widget = QWidget()
+        #self.select_crop_widget.setLayout(self.initial_layout)
+        #self.select_crop_widget.setLayout(self.initial_layout)
 
-        #self.central_widget = QWidget()
-        #self.central_widget.setLayout(self.stackedLayout)
-        #self.setCentralWidget(self.central_widget)
-
-        
 
         self.setWindowTitle('DollyProject')
         self.show()
 
-   def create_mainwindows_layout(self):
-        self.lbl = QtGui.QLabel(self)
-        pixmap = QtGui.QPixmap("img/logo_menu.png")
+   def create_mainwindow_layout(self):
+        self.main_window_layout = QWidget()
+        self.lbl = QLabel(self)
+        pixmap = QPixmap("img/logo_menu.png")
         self.lbl.setPixmap(pixmap)
         self.lbl.move(140, 30)
-
-        self.howto = QtGui.QLabel(self)
-        self.howto.setGeometry( 0,0,480,320 )
+        #self.main_window_layout.addWidget(self.lbl)
 
 
-        btn1 = QtGui.QPushButton("", self)
-        btn1.setIcon(QtGui.QIcon('img/mode_1.png'))
-        btn1.setIconSize(QtCore.QSize(138,145))
+        btn1 = QPushButton("", self)
+        btn1.setIcon(QIcon('img/mode_1.png'))
+        btn1.setIconSize(QSize(138,145))
         btn1.resize(138,145)
         btn1.move(20, 90)
-        self.connect(btn1,QtCore.SIGNAL("clicked()"), self.howto1)
+        self.connect(btn1,SIGNAL("clicked()"), self.howto1)
 
-        btn2 = QtGui.QPushButton("", self)
-        btn2.setIcon(QtGui.QIcon('img/mode_2.png'))
-        btn2.setIconSize(QtCore.QSize(138,145))
+        btn2 = QPushButton("", self)
+        btn2.setIcon(QIcon('img/mode_2.png'))
+        btn2.setIconSize(QSize(138,145))
         btn2.resize(138,145)
         btn2.move(170, 90)
 
-        btn3 = QtGui.QPushButton("", self)
-        btn3.setIcon(QtGui.QIcon('img/mode_3.png'))
-        btn3.setIconSize(QtCore.QSize(138,145))
+        btn3 = QPushButton("", self)
+        btn3.setIcon(QIcon('img/mode_3.png'))
+        btn3.setIconSize(QSize(138,145))
         btn3.resize(138,145)
         btn3.move(320, 90)
 
-        about = QtGui.QPushButton("", self)
-        about.setIcon(QtGui.QIcon('img/info.png'))
-        about.setIconSize(QtCore.QSize(53,53))
+        about = QPushButton("", self)
+        about.setIcon(QIcon('img/info.png'))
+        about.setIconSize(QSize(53,53))
         about.resize(53,53)
         about.move(10, 260)
         about.clicked.connect(self.buttonClicked)
 
-        close = QtGui.QPushButton("", self)
-        close.setIcon(QtGui.QIcon('img/power.png'))
-        close.setIconSize(QtCore.QSize(53,53))
+        close = QPushButton("", self)
+        close.setIcon(QIcon('img/power.png'))
+        close.setIconSize(QSize(53,53))
         close.resize(53,53)
         close.move(420, 260)
-        close.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        close.clicked.connect(QCoreApplication.instance().quit)
 
         self.setGeometry(0, 0, 480, 320)
 
    def buttonClicked(self):
-         msgBox = QtGui.QMessageBox()
+         msgBox = QMessageBox()
          msgBox.setText("Created by\nNipitpon Chantada\n5533684623")
-         msgBox.setIcon(QtGui.QMessageBox.Information)
+         msgBox.setIcon(QMessageBox.Information)
          retval = msgBox.exec_()
          print "value of pressed message box button:", retval
 
    def howto1(self):
        print "howto1 screen"
-       pixmap = QtGui.QPixmap("img/howto_1.png")
+       self.howto = QLabel(self)
+       self.howto.setGeometry( 0,0,480,320 )
+       pixmap = QPixmap("img/howto_1.png")
        self.howto.setPixmap(pixmap)
        self.howto.move(0,0)
 
 
 def main():
 
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
+    app = QApplication(sys.argv)
+    ex = DollyProject()
     sys.exit(app.exec_())
 
 
