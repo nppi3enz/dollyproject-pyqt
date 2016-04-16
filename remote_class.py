f2a515dac96e6d85fc78cd12d0b7d44bf141b548
nppi3enz <nppi3enz@gmail.com>
2016-04-17 01:05:01 +0700

    subtract motor class

--- remote_class.py
+++ remote_class.py
@@ -3,58 +3,11 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 from main import StackedWidget
+from motor_class import *
 
 import microgear.client as client
 import time
 
-#for motor
-#import time
-import atexit
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-
-class motor(QThread):
-    myMotor = pyqtSignal(object)
-    #global mySpeed
-    #mySpeed = 100
-
-    def __init__(self):
-        self.mySpeed = 100
-
-        #print ("initial motor at "+str(mySpeed))
-        print ("initial motor at "+str(self.mySpeed))
-
-        self.mh = Adafruit_MotorHAT(addr=0x60)
-        atexit.register(self.turnOffMotors)
-        self.myMotor_front = self.mh.getMotor(3)
-        #self.myMotor_front.setSpeed(mySpeed)
-
-    def turnOffMotors(self):
-        print "Off Motor Complete"
-    	self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    	self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    	self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    	self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
-    def left(self):
-        self.myMotor_front.run(Adafruit_MotorHAT.FORWARD)
-        print "\tSpeed up...to"+str(self.mySpeed)
-        self.myMotor_front.setSpeed(self.mySpeed)
-        time.sleep(0.01)
-
-    def right(self):
-        self.myMotor_front.run(Adafruit_MotorHAT.BACKWARD)
-        print "\tSpeed up...to"+str(self.mySpeed)
-        self.myMotor_front.setSpeed(self.mySpeed)
-        time.sleep(0.01)
-
-    def stop(self):
-        self.turnOffMotors()
-
-    def setmySpeed(self, speed):
-        self.turnOffMotors()
-        print ("new set speed = "+str(speed))
-        #self.mySpeed = speed
-        self.mySpeed = int(speed)
 
 class netpie(QThread):
     mySignal = pyqtSignal(object)
