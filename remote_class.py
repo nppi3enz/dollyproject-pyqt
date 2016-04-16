1dec9016ea0cd7a184b6cee27868a4e5b232a9b8
nppi3enz <nppi3enz@gmail.com>
2016-04-16 18:42:53 +0700

    add feature remote control

--- remote_class.py
+++ remote_class.py
@@ -6,6 +6,56 @@ from main import StackedWidget
 
 import microgear.client as client
 import time
+
+#for motor
+#import time
+import atexit
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+class motor(QThread):
+    myMotor = pyqtSignal(object)
+    #global mySpeed
+    #mySpeed = 100
+
+    def __init__(self):
+        self.mySpeed = 100
+
+        #print ("initial motor at "+str(mySpeed))
+        print ("initial motor at "+str(self.mySpeed))
+
+        self.mh = Adafruit_MotorHAT(addr=0x60)
+        atexit.register(self.turnOffMotors)
+        self.myMotor_front = self.mh.getMotor(3)
+        #self.myMotor_front.setSpeed(mySpeed)
+
+    def turnOffMotors(self):
+        print "Off Motor Complete"
+    	self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    	self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    	self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    	self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+    def left(self):
+        self.myMotor_front.run(Adafruit_MotorHAT.FORWARD)
+        print "\tSpeed up...to"+str(self.mySpeed)
+        self.myMotor_front.setSpeed(self.mySpeed)
+        time.sleep(0.01)
+
+    def right(self):
+        self.myMotor_front.run(Adafruit_MotorHAT.BACKWARD)
+        print "\tSpeed up...to"+str(self.mySpeed)
+        self.myMotor_front.setSpeed(self.mySpeed)
+        time.sleep(0.01)
+
+    def stop(self):
+        self.turnOffMotors()
+
+    def setmySpeed(self, speed):
+        self.turnOffMotors()
+        print ("new set speed = "+str(speed))
+        #self.mySpeed = speed
+        self.mySpeed = int(speed)
+
 class netpie(QThread):
     mySignal = pyqtSignal(object)
 
@@ -14,7 +64,8 @@ class netpie(QThread):
         self.welcome()
         self.mySignal.emit('load')
         self.create_client()
-        #self.start_connect()
+        self.start_connect() #start when push button
+
 
     def create_client(self):
        gearkey = 'K0v7QsvD5AgzE0n'
@@ -52,13 +103,18 @@ class netpie(QThread):
 class Remote(QWidget):
     #constructor
     def __init__(self, stack):
+
         #super(Remote, self).__init__()
         QWidget.__init__(self, stack)
         #self.stack = stack
 
+        #netpie class
         self.net = netpie()
         self.initUI(stack)
         self.net.mySignal.connect(self.fetch_network)
+
+        #motor class
+        self.motor = motor()
         #self.net.start_connect()
 
     def initUI(self, stack):
@@ -111,10 +167,13 @@ class Remote(QWidget):
         print ('get MSG : '+msg)
         if msg == 'LEFT':
             pixmap = QPixmap("img/status_left.png")
+            self.motor.left()
         elif msg == 'RIGHT':
             pixmap = QPixmap("img/status_right.png")
+            self.motor.right()
         elif msg == 'STOP':
             pixmap = QPixmap("img/status_stop.png")
+            self.motor.stop()
         elif msg == 'load':
             pixmap = QPixmap("img/status_connecting.png")
         else:
@@ -122,6 +181,7 @@ class Remote(QWidget):
             slist = msg.split("|")
             print (slist[1])
             self.numMotor.setText(slist[1])
+            self.motor.setmySpeed(slist[1])
             pixmap = QPixmap("img/status_stop.png")
 
 
